42d33a6ba3e2b2b6472cd7e99e493eaf8bfc05c7
Michael Veeck <veeck@neofonie.de>
2018-06-21 15:04:38 +0200

    Add (non working) nsis scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,10 @@ module.exports = function (grunt) {
     'use strict';
 
     require('time-grunt')(grunt);
-    require('load-grunt-tasks')(grunt);
+
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-makensis');
+    grunt.loadNpmTasks('grunt-nw-builder');
 
     grunt.initConfig({
         nwjs: {
@@ -30,10 +33,23 @@ module.exports = function (grunt) {
                     dest: '/gCodeViewer-gui'
                 }]
             }
+        },
+        makensis: {
+            options: {
+                srcDir: './dist/gCodeViewer/win64/',
+                buildDir: './dist/gCodeViewer/exe/',
+                appName: 'gCodeViewer',
+                setupName: '_installer'
+            }
         }
     });
 
-    grunt.registerTask('default', [
+    grunt.registerTask('exe', [
+        'nwjs',
+        'makensis'
+    ]);
+
+    grunt.registerTask('zip', [
         'nwjs',
         'compress'
     ]);
